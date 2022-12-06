cdb98f9d23847825a6ebdff12d1d173851ff02d9
Vladimir Potapov <vovan4ik.potapov@gmail.com>
2022-12-07 01:09:27 +0300

    Added some changes for user part and removed unnecessary

--- src/main/java/com/my/shared/Node.java
+++ src/main/java/com/my/shared/Node.java
@@ -7,46 +7,13 @@ import java.util.List;
 
 public class Node implements IsSerializable {
     private String data = "";
-    private int id;
-    private static int idCounter = 0;
     private List<Node> children = new ArrayList<>();
-    private int parentId;
 
     public Node() {
-        this.id = idCounter++;
-        parentId = -1;
     }
 
     public Node(String data) {
-        this.id = idCounter++;
         this.data = data;
-        parentId = -1;
-    }
-
-    public Node(String data, int parentId) {
-        this.id = idCounter++;
-        this.data = data;
-        this.parentId = parentId;
-    }
-
-    public static void clearIdCounter() {
-        idCounter = 0;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(int parentId) {
-        this.parentId = parentId;
     }
 
     public void setData(String data) {
@@ -65,27 +32,4 @@ public class Node implements IsSerializable {
         return children;
     }
 
-    public List<Node> recursiveFindAllChildren() {
-        List<Node> resultList = new ArrayList<>();
-        for (Node node : children) {
-            resultList.add(node);
-            List<Node> nodeChildren = node.getTopChildren();
-            if (nodeChildren.size() > 0) {
-                recursiveFindAllChildren(node, resultList);
-            }
-        }
-        return resultList;
-    }
-
-    private void recursiveFindAllChildren(Node tree, List<Node> resultList) {
-        List<Node> children = tree.getTopChildren();
-        for (int i = 0; i < children.size(); i++) {
-            Node node = children.get(i);
-            resultList.add(node);
-            List<Node> nodeChildren = tree.getTopChildren();
-            if (nodeChildren.size() > 0) {
-                recursiveFindAllChildren(node, resultList);
-            }
-        }
-    }
 }
